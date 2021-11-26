9be692049de70cbe8e595ab9aa858442f9a662b0
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-26 17:27:32 -0500

    added posts container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './containers/Home';
+import Posts from './containers/Posts';
 
 function App() {
 	return (
 		<>
 			<div className="App">blog test</div>
 			<Home />
+			<Posts />
 		</>
 	);
 }
---
+++ src/containers/Posts.js
@@ -0,0 +1,4 @@
+function Posts(props) {
+	return <h1>This is where all the posts will be displayed</h1>;
+}
+export default Posts;
