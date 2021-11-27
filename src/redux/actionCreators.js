b7ad4a1b9fa4cb060f22916370e3b040fc3cf7c2
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-27 17:40:26 -0500

    fetch a single post

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -4,3 +4,9 @@ export const getPosts = () => {
 			.then((res) => res.json())
 			.then((posts) => dispatch({ type: 'GET_POSTS', payload: posts }));
 };
+export const getPost = (id) => {
+	return (dispatch) =>
+		fetch(`http://localhost:3000/posts/${id}`)
+			.then((res) => res.json())
+			.then((post) => dispatch({ type: 'GET_POST', payload: post }));
+};
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,18 @@
 const initialState = {
 	posts: [],
+	selectedPost: {
+		imageUrl: '',
+		title: '',
+		description: '',
+	},
 };
 export function reducer(state = initialState, action) {
 	switch (action.type) {
 		case 'GET_POSTS':
 			return { ...state, posts: action.payload };
+		case 'GET_POST':
+			return { ...state, selectedPost: action.payload };
+
 		default:
 			return { ...state };
 	}
