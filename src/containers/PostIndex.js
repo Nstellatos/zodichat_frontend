bdc9b6d701bb72da2406ba5f028a5baf440e70dd
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-11-27 14:52:06 -0500

    successful fetch to db

---
+++ src/containers/PostIndex.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { getPosts } from '../redux/actionCreators';
+import { connect } from 'react-redux';
+import PostCard from '../components/PostCard';
+
+function PostIndex({ getPosts, posts }) {
+	useEffect(getPosts, [getPosts]);
+
+	return (
+		<div className="posts">
+			{posts.map((post) => (
+				<PostCard {...post} key={post.id} />
+			))}
+		</div>
+	);
+}
+
+const mapStateToProps = (state) => {
+	return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { getPosts })(PostIndex);
--- src/containers/Posts.js
+++
@@ -1,4 +0,0 @@
-function Posts(props) {
-	return <h1>This is where all the posts will be displayed</h1>;
-}
-export default Posts;
