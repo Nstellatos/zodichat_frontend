fe82248af1893c0b08783100c867affc5b89a77b
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-05 00:02:08 -0500

    created show page for a sign

--- src/components/SignShow.js
+++ src/components/SignShow.js
@@ -1,4 +1,39 @@
-function SignShow(props) {
-	return <h1>This is the show page for a sign</h1>;
+import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getSign, clearSign } from '../redux/actionCreators';
+import { useEffect } from 'react';
+import { Posts } from './';
+
+function SignShow({
+	getSign,
+	name,
+	imageUrl,
+
+	clearSign,
+	id,
+}) {
+	const routeId = useParams().id;
+
+	useEffect(() => {
+		getSign(routeId);
+		// if you return a function in your useEffect function, it will run that when the component is about to unmount
+		return clearSign;
+	}, [getSign, routeId, clearSign]);
+
+	const spinner = () => <div className="loader"></div>;
+	const loadedPage = () => (
+		<div className="show">
+			<img src={imageUrl} alt={name} />
+			<p>{name}</p>
+			<Posts />
+		</div>
+	);
+
+	return id ? loadedPage() : spinner();
 }
-export default SignShow;
+
+const mapStateToProps = (state) => {
+	return { ...state.selectedSign };
+};
+
+export default connect(mapStateToProps, { getSign, clearSign })(SignShow);
