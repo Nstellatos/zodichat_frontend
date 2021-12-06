0f0f95af5176807b1c967ad883621d7df4c6dcef
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-05 21:58:06 -0500

    export PostShow

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export { default as SignShow } from './SignShow';
 export { default as SignIndex } from '../containers/SignIndex';
 
 export { default as Nav } from './Nav';
-
 export { default as PostCard } from './PostCard';
+
+export { default as PostShow } from './PostShow';
+
 export { default as Posts } from '../containers/Posts';
