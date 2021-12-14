f43f30a79b03315b81d0c014817e51374ad7b25e
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-14 16:41:45 -0500

    fetch to get all posts

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -12,9 +12,34 @@ export const getSign = (id) => {
 };
 export const clearSign = () => ({ type: 'CLEAR_SIGN' });
 
+export const getPosts = () => {
+	return (dispatch) =>
+		fetch('http://localhost:3000/posts')
+			.then((res) => res.json())
+			.then((posts) => dispatch({ type: 'GET_POSTS', payload: posts }));
+};
+
 export const getPost = (id) => {
 	return (dispatch) =>
-		fetch(`http://localhost:3000/posts/${id}`)
+		fetch(`http://localhost:3000/signs/:sign_id/posts`)
 			.then((res) => res.json())
 			.then((post) => dispatch({ type: 'GET_POST', payload: post }));
+};
+export const submitPost = (post) => {
+	return (dispatch) =>
+		fetch(`http://localhost:3000/posts`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(post),
+		}).then((res) => {
+			if (res.ok) {
+				res
+					.json()
+					.then((post) => dispatch({ type: 'ADD_POST', payload: post }));
+			} else {
+				res.json().then((res) => alert(res.errors));
+			}
+		});
 };
