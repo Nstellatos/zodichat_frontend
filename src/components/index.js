2de1bafd5d872b45974b4415b8ac7eff7ce65e08
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-20 21:23:17 -0500

    removed signIndex import

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as SignCard } from './SignCard';
 export { default as SignShow } from './SignShow';
-export { default as SignIndex } from '../containers/SignIndex';
 
 export { default as Nav } from './Nav';
 export { default as PostCard } from './PostCard';
