752f50fa30fb2727a40f0d6f04fa625acdeb8ced
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-21 15:40:37 -0500

    added auth to exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as SignShow } from './SignShow';
 export { default as Nav } from './Nav';
 export { default as PostCard } from './PostCard';
 export { default as PostForm } from './PostForm';
+export { default as Auth } from './Auth';
 
 export { default as Posts } from '../containers/Posts';
 export { default as Home } from '../containers/Home';
