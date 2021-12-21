3522939d9a2df55972d3c68cd6576d1f4a2514cc
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-20 21:53:39 -0500

    action for submitting post

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -12,25 +12,13 @@ export const getSign = (id) => {
 };
 export const clearSign = () => ({ type: 'CLEAR_SIGN' });
 
-export const getPosts = () => {
+export const submitPost = (post, signId) => {
 	return (dispatch) =>
-		fetch('http://localhost:3000/posts')
-			.then((res) => res.json())
-			.then((posts) => dispatch({ type: 'GET_POSTS', payload: posts }));
-};
-
-export const getPost = (id) => {
-	return (dispatch) =>
-		fetch(`http://localhost:3000/signs/:sign_id/posts`)
-			.then((res) => res.json())
-			.then((post) => dispatch({ type: 'GET_POST', payload: post }));
-};
-export const submitPost = (post) => {
-	return (dispatch) =>
-		fetch(`http://localhost:3000/posts`, {
+		fetch(`http://localhost:3000/signs/${signId}/posts`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
+				Authorization: localStorage.token,
 			},
 			body: JSON.stringify(post),
 		}).then((res) => {
