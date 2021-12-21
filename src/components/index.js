0b207e9f1eff66a2293088832b73e1d31ed20ff6
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-20 21:38:47 -0500

    removed unnessary components

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,6 @@ export { default as SignShow } from './SignShow';
 export { default as Nav } from './Nav';
 export { default as PostCard } from './PostCard';
 export { default as PostForm } from './PostForm';
-export { default as PostShow } from './PostShow';
 
 export { default as Posts } from '../containers/Posts';
 export { default as Home } from '../containers/Home';
