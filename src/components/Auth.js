8d55861437cdfd235088080c8f4f728ce3c2823e
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-21 15:40:20 -0500

    basic signup/login form

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import { useState } from 'react';
 
-export default function Auth() {
-	return <div>auth will go here!</div>;
+function Auth(props) {
+	const [signup, setSignup] = useState(false);
+	const toggleSignup = () => setSignup(!signup);
+	return (
+		<>
+			{signup ? <h1>Sign up!</h1> : <h1>Login!</h1>}
+			<form>
+				<label>
+					Username:
+					<input type="text" name="username" />
+				</label>
+				<label>
+					Password:
+					<input type="password" name="password" />
+				</label>
+			</form>
+			<button onClick={toggleSignup}> or {signup ? 'Login' : 'Signup'}</button>
+		</>
+	);
 }
+export default Auth;
