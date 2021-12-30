578349966f75330c83e0c2038930c55f323ff619
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2021-12-30 00:14:28 -0500

    replaced material ui w/ react-modal

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,61 +1,67 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 
-const ExpandMore = styled((props) => {
-	const { expand, ...other } = props;
-	return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-	transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-	marginLeft: 'auto',
-	transition: theme.transitions.create('transform', {
-		duration: theme.transitions.duration.shortest,
-	}),
-}));
-
-export default function PostCard({ title, description, imageUrl, username }) {
-	const [expanded, setExpanded] = React.useState(false);
-
-	const handleExpandClick = () => {
-		setExpanded(!expanded);
-	};
-
+function PostCard({ id, title, description, imageUrl, username }) {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<>
-			<Card className="post" sx={{ maxWidth: 345 }}>
-				<CardMedia component="img" height="194" image={imageUrl} alt={title} />
-				<CardHeader title={title} subheader={username} />
-				<CardContent>
-					<p className="postDesc">{description}</p>
-				</CardContent>
-				<CardActions disableSpacing>
-					View Post
-					<ExpandMore
-						expand={expanded}
-						onClick={handleExpandClick}
-						aria-expanded={expanded}
-						aria-label="show more"
-					>
-						<ExpandMoreIcon />
-					</ExpandMore>
-				</CardActions>
-				<Collapse in={expanded} timeout="auto" unmountOnExit>
-					<CardContent>{description}</CardContent>
-				</Collapse>
-			</Card>
-		</>
+		<div className="post">
+			<img className="postImg" src={imageUrl} alt={title} />
+			<div className="postInfo">
+				<span className="postTitle">
+					<h3>{title}</h3>
+				</span>
+
+				<p className="postDesc">{description}</p>
+			</div>
+
+			<button onClick={() => setIsOpen(true)}>Open</button>
+			<Modal
+				className="main"
+				isOpen={isOpen}
+				onRequestClose={() => setIsOpen(false)}
+				style={{
+					overlay: {
+						backgroundColor: 'black',
+					},
+					content: {
+						backgroundColor: 'white',
+					},
+				}}
+			>
+				<img className="postImg" src={imageUrl} alt={title} />
+				<div className="postInfo">
+					<span className="postTitle">
+						<h3>{title}</h3>
+
+						<Link to={`/users/${id}`}>
+							<h4>{username}</h4>
+						</Link>
+					</span>
+					<p>{description}</p>
+				</div>
+				<button onClick={() => setIsOpen(false)}>Close</button>
+			</Modal>
+		</div>
 	);
 }
+export default PostCard;
+/*
+function PostCard({ id, title, description, imageUrl }) {
+	return (
+		<div className="post">
+			<img className="postImg" src={imageUrl} alt={title} />
+			<div className="postInfo">
+				<span className="postTitle">
+					<h3>{title}</h3>
+				</span>
+			</div>
+
+			
+				<p>{description}</p>
+			
+		</div>
+	);
+}
+export default PostCard;
+*/
