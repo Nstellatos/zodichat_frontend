809ad812995ac853a2d5bff97eb86c66a4a7941b
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-02 15:51:23 -0500

    removed link

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
-import { Link } from 'react-router-dom';
 
 function PostCard({ id, title, description, imageUrl, username }) {
 	const [isOpen, setIsOpen] = useState(false);
