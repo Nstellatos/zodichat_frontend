6749024512e598c8c744b8569abef320b1e54877
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 15:23:01 -0500

    container for all post instead of sign posts

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,18 +1,29 @@
+import { useEffect } from 'react';
+import { getPosts } from '../redux/actionCreators';
 import { connect } from 'react-redux';
 import { PostCard } from '../components';
 
-function Posts({ posts }) {
+function Posts({ getPosts, posts }) {
+	useEffect(getPosts, [getPosts]);
+
 	return (
-		<div className="posts">
-			{posts.map((post) => (
-				<PostCard {...post} key={post.id} />
-			))}
+		<div className="container my-2 py-2">
+			<div className="row">
+				<div className="col-12 mb-5">
+					<hr />
+				</div>
+			</div>
+			<div className="row justify-content-center">
+				{posts.map((post) => (
+					<PostCard {...post} key={post.id} />
+				))}
+			</div>
 		</div>
 	);
 }
 
-const mapStateToProps = (state) => ({
-	posts: state.selectedSign.posts,
-});
+const mapStateToProps = (state) => {
+	return { posts: state.posts };
+};
 
-export default connect(mapStateToProps)(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
