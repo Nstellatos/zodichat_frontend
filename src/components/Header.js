7f4f86222ea510f6016ed94789b6b061110303b6
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 14:55:47 -0500

    changed video for header. added bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import { Stars } from '../components';
-
-const Header = () => {
+export default function Header() {
 	return (
-		<div className="header">
-			<div className="headerTitles">
-				<span className="headerTitleSm">WELCOME TO</span>
-				<span className="headerTitleLg">ZODICHAT</span>
-
-				<video className="headerImg" autoPlay loop muted>
+		<div className="hero">
+			<div className="card bg-dark text-white border-0">
+				<video className="card-img" autoPlay loop muted>
 					<source src={Stars} type="video/mp4" />
 				</video>
+				<div className="card-img-overlay d-flex flex-column justify-content-center">
+					<div className="container">
+						<h5 className="card-title display-3 fw-bolder mb-0">ZODICHAT</h5>
+						<p className="card-text lead fs-2">WHERE THE SIGNS COME TO CHAT</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
-};
-export default Header;
+}
