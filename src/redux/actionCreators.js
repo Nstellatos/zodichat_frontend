7f02fb259179fdd31bf9cb89359b00596d77835f
Nstellatos <47094369+Nstellatos@users.noreply.github.com>
2022-01-14 15:05:29 -0500

    added fetch to getPosts & getPost

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -14,7 +14,7 @@ export const clearSign = () => ({ type: 'CLEAR_SIGN' });
 
 export const submitSignup = (user) => {
 	return (dispatch) =>
-		fetch('http://localhost:3000//users', {
+		fetch('http://localhost:3000/users', {
 			method: 'POST', // or 'PUT'
 			headers: {
 				'Content-Type': 'application/json',
@@ -78,3 +78,22 @@ function handleUserResponse(res, dispatch) {
 		res.json().then((res) => alert(res.errors));
 	}
 }
+export const getPosts = (signId) => {
+	return (dispatch) =>
+		fetch(`http://localhost:3000/signs/${signId}/posts`)
+			.then((res) => res.json())
+			.then((posts) => dispatch({ type: 'GET_POSTS', payload: posts }));
+};
+
+export const getPost = (id) => {
+	return (dispatch) =>
+		fetch(`http://localhost:3000/posts/${id}`)
+			.then((res) => res.json())
+			.then((post) => dispatch({ type: 'GET_POST', payload: post }));
+};
+export const getUsers = () => {
+	return (dispatch) =>
+		fetch('http://localhost:3000/users')
+			.then((res) => res.json())
+			.then((users) => dispatch({ type: 'GET_USERS', payload: users }));
+};
